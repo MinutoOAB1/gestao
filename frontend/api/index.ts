b37d39bb577c28e5d5062948f07d59ae1522ee92
Vinyel Victor Campos Leite <viiny36@gmail.com>
2026-04-10 16:44:48 -0400

    fix(api): use default import for express with esModuleInterop

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from '../../backend/src/app.module';
-import * as express from 'express';
+import express from 'express';
 
 // @ts-ignore
 const compression = require('compression');
